9b9ceeb9b4f95f2cc5e1e7ee6c10f86dee8cfbf5
Aram Hammoudeh <aram.hammoudeh@indr.com>
2024-10-29 17:45:35 -0600

    remove game

--- samples/BasicSample/main.cpp
+++ samples/BasicSample/main.cpp
@@ -1,19 +1,15 @@
 #include "Engine.h"
-#include "SDL2_OpenGLRenderer.h"
 
 using namespace GoudEngine;
 
 int main()
 {
     Engine engine;
-    SDL2_OpenGLRenderer renderer; // Use the concrete renderer here
 
     engine.Initialize();
-    renderer.Initialize();
 
     engine.Run();
 
-    renderer.Shutdown();
     engine.Shutdown();
 
     return 0;
