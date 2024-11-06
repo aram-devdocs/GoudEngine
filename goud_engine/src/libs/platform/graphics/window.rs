373f336652c4ba1bfa44b365c908f82dcbbde0c0
Aram Hammoudeh <aram.hammoudeh@indr.com>
2024-11-06 14:12:46 -0700

    maintain aspect ratio

--- goud_engine/src/libs/platform/graphics/window.rs
+++ goud_engine/src/libs/platform/graphics/window.rs
@@ -65,6 +65,7 @@ impl Window {
 
         window.set_framebuffer_size_polling(true);
         window.set_key_polling(true);
+        window.set_size_polling(true); // Add size polling
 
         Window {
             glfw,
@@ -89,17 +90,37 @@ impl Window {
         self.process_events();
         self.glfw.poll_events();
         self.window_handle.swap_buffers();
+        self.maintain_aspect_ratio(); // Maintain aspect ratio on update
     }
 
     pub fn is_key_pressed(&self, key: Key) -> bool {
         self.input_handler.is_key_pressed(key)
     }
 
+
+    // TODO: Is this going to add a lot of overhead?
+    fn maintain_aspect_ratio(&mut self) {
+        let (current_width, current_height) = self.window_handle.get_size();
+        let aspect_ratio = self.width as f32 / self.height as f32;
+        let new_width = (current_height as f32 * aspect_ratio).round() as i32;
+        let new_height = (current_width as f32 / aspect_ratio).round() as i32;
+
+        if new_width != current_width {
+            self.window_handle.set_size(new_width, current_height);
+        } else if new_height != current_height {
+            self.window_handle.set_size(current_width, new_height);
+        }
+    }
+
     fn process_events(&mut self) {
-        for (_, event) in glfw::flush_messages(&self.events) {
+        let events: Vec<_> = glfw::flush_messages(&self.events).collect();
+        for (_, event) in events {
             self.input_handler.handle_event(&event);
             if let WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
                 self.window_handle.set_should_close(true);
+            }
+            if let WindowEvent::Size(width, height) = event {
+                self.maintain_aspect_ratio(); // Maintain aspect ratio on resize
             }
         }
     }
