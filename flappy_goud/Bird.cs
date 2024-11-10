4719822417119c226a7588974dd751785a2fcdda
Aram Hammoudeh <aram.hammoudeh@indr.com>
2024-11-10 10:20:43 -0700

    add bird animator

--- flappy_goud/Bird.cs
+++ flappy_goud/Bird.cs
@@ -1,14 +1,11 @@
 // Bird.cs
 
-using System;
-using CsBindgen;
-
 public class Bird
 {
     private readonly GoudGame game;
     private readonly Movement movement;
+    private readonly BirdAnimator animator;
 
-    private uint spriteIndex;
     public float X { get; private set; }
     public float Y { get; private set; }
 
@@ -18,15 +15,12 @@ public class Bird
         this.movement = new Movement(GameConstants.Gravity, GameConstants.JumpStrength);
         this.X = GameConstants.ScreenWidth / 4;
         this.Y = GameConstants.ScreenHeight / 2;
+        this.animator = new BirdAnimator(game, X, Y);
     }
 
     public void Initialize()
     {
-        spriteIndex = game.AddSprite("assets/sprites/bluebird-midflap.png", new SpriteDto
-        {
-            x = X,
-            y = Y,
-        });
+        animator.Initialize();
     }
 
     public void Reset()
@@ -34,6 +28,7 @@ public class Bird
         X = GameConstants.ScreenWidth / 4;
         Y = GameConstants.ScreenHeight / 2;
         movement.Velocity = 0;
+        animator.Reset();
     }
 
     public void Update(float deltaTime)
@@ -48,14 +43,12 @@ public class Bird
         movement.UpdatePosition(ref yPosition, deltaTime);
         Y = yPosition;
 
-        // Update the bird's position in the game
-        game.UpdateSprite(spriteIndex, new SpriteDto { x = X, y = Y, rotation = movement.Rotation });
+        // Update the animator with the new position and rotation
+        animator.Update(deltaTime, X, Y, movement.Rotation);
     }
 
     public bool CollidesWith(Pipe pipe)
     {
         return pipe.Intersects(X, Y, GameConstants.BirdWidth, GameConstants.BirdHeight);
     }
-
-
 }
---
+++ flappy_goud/BirdAnimator.cs
@@ -0,0 +1,68 @@
+// BirdAnimator.cs
+
+using System;
+using System.Collections.Generic;
+
+using CsBindgen;
+public class BirdAnimator
+{
+    private readonly GoudGame game;
+    private readonly List<string> spritePaths;
+    private int currentFrame;
+    private float animationTime;
+    private readonly float frameDuration;
+    private float initialX;
+    private float initialY;
+
+    public uint SpriteIndex { get; private set; }
+
+    public BirdAnimator(GoudGame game, float x, float y, float frameDuration = 0.1f)
+    {
+        this.game = game;
+        this.frameDuration = frameDuration;
+        this.animationTime = 0;
+        this.currentFrame = 0;
+        this.initialX = x;
+        this.initialY = y;
+
+        // Load the animation frames
+        spritePaths = new List<string>
+        {
+            "assets/sprites/bluebird-downflap.png",
+            "assets/sprites/bluebird-midflap.png",
+            "assets/sprites/bluebird-upflap.png"
+        };
+    }
+
+    public void Initialize()
+    {
+        // Set up the initial sprite
+        SpriteIndex = game.AddSprite(spritePaths[currentFrame], new SpriteDto { x = initialX, y = initialY });
+    }
+
+    public void Update(float deltaTime, float x, float y, float rotation)
+    {
+        animationTime += deltaTime;
+
+        // Update frame if the animation time exceeds the frame duration
+        if (animationTime >= frameDuration)
+        {
+            currentFrame = (currentFrame + 1) % spritePaths.Count;
+            game.RemoveSprite(SpriteIndex);
+            SpriteIndex = game.AddSprite(spritePaths[currentFrame], new SpriteDto { x = x, y = y });
+            animationTime = 0;
+        }
+        else
+        {
+            // Update only position and rotation if within the same frame
+            game.UpdateSprite(SpriteIndex, new SpriteDto { x = x, y = y, rotation = rotation });
+        }
+    }
+
+    public void Reset()
+    {
+        currentFrame = 0;
+        animationTime = 0;
+        game.UpdateSprite(SpriteIndex, new SpriteDto { x = initialX, y = initialY });
+    }
+}
