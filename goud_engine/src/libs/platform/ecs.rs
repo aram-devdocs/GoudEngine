3d4694113aec1d02e52fdddd268b24ecde7eb98c
Aram Hammoudeh <aram.hammoudeh@indr.com>
2024-11-13 14:45:26 -0700

    refactor ECS to use BTreeMap for sprite management and remove unused methods

--- goud_engine/src/libs/platform/ecs.rs
+++ goud_engine/src/libs/platform/ecs.rs
@@ -3,13 +3,6 @@ use std::{collections::BTreeMap, ffi::c_uint};
 use crate::types::{Sprite, SpriteCreateDto, SpriteUpdateDto};
 
 pub struct ECS {
-    // pub sprites: Vec<Option<Sprite>>, // Vec for storing sprites with optional entries
-    // store as BTree to allow for ordering by z_layer
-    // should we use a BTreeMap or a BTreeSet?
-    // BTreeMap allows for duplicate z_layer values
-    // BTreeSet does not allow for duplicate z_layer values
-    // well we need to allow for duplicate z_layer values
-    // so we should use a BTreeMap
     pub sprites: BTreeMap<i32, Vec<Sprite>>,
     next_id: c_uint,        // Tracks the next unused ID
     free_list: Vec<c_uint>, // List of reusable indices
@@ -75,20 +68,6 @@ impl ECS {
         }
     }
 
-    /// Updates a sprite for a given EntityId.
-    // pub fn update_sprite(&mut self, entity_id: EntityId, sprite: Sprite) -> Result<(), String> {
-    //     if let Some(slot) = self.sprites.get_mut(entity_id as usize) {
-    //         if slot.is_some() {
-    //             *slot = Some(sprite);
-    //             Ok(())
-    //         } else {
-    //             Err("EntityId not found".into())
-    //         }
-    //     } else {
-    //         Err("EntityId not found".into())
-    //     }
-    // }
-
     // refactor using BTreeMap
     pub fn update_sprite(&mut self, sprite_input: SpriteUpdateDto) -> Result<(), String> {
         let sprite = Sprite::new(
@@ -114,13 +93,6 @@ impl ECS {
         Err("EntityId not found".into())
     }
 
-    /// Retrieves a sprite by EntityId, if it exists.
-    // pub fn get_sprite(&self, entity_id: EntityId) -> Option<&Sprite> {
-    //     self.sprites
-    //         .get(entity_id as usize)
-    //         .and_then(|slot| slot.as_ref())
-    // }
-
     // refactor using BTreeMap
     pub fn get_sprite(&self, sprite_id: c_uint) -> Option<&Sprite> {
         for sprites in self.sprites.values() {
@@ -131,17 +103,6 @@ impl ECS {
         None
     }
 
-    /// Removes a sprite by EntityId.
-    // pub fn remove_sprite(&mut self, entity_id: EntityId) -> Result<Sprite, String> {
-    //     if let Some(slot) = self.sprites.get_mut(entity_id as usize) {
-    //         if let Some(sprite) = slot.take() {
-    //             self.free_list.push(entity_id); // Mark index as reusable
-    //             return Ok(sprite);
-    //         }
-    //     }
-    //     Err("EntityId not found".into())
-    // }
-
     // refactor using BTreeMap
     pub fn remove_sprite(&mut self, sprite_id: c_uint) -> Result<Sprite, String> {
         for sprites in self.sprites.values_mut() {
@@ -151,24 +112,6 @@ impl ECS {
         }
         Err("EntityId not found".into())
     }
-
-    // pub fn check_collision_between_sprites(
-    //     &self,
-    //     entity_id1: EntityId,
-    //     entity_id2: EntityId,
-    // ) -> bool {
-    //     match (
-    //         self.sprites
-    //             .get(entity_id1 as usize)
-    //             .and_then(|s| s.as_ref()),
-    //         self.sprites
-    //             .get(entity_id2 as usize)
-    //             .and_then(|s| s.as_ref()),
-    //     ) {
-    //         (Some(sprite1), Some(sprite2)) => sprite1.check_collision(sprite2),
-    //         _ => false,
-    //     }
-    // }
 
     // refactor using BTreeMap
     pub fn check_collision_between_sprites(&self, sprite_id1: c_uint, sprite_id2: c_uint) -> bool {
