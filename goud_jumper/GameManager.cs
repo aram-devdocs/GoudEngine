4397e9037204984b815c5a473b9816519d7ca6c4
Aram Hammoudeh <aram.hammoudeh@indr.com>
2024-11-18 11:38:53 -0700

    add goudjumper app

---
+++ goud_jumper/GameManager.cs
@@ -0,0 +1,20 @@
+// GameManager.cs
+
+using System;
+using System.Collections.Generic;
+using CsBindgen;
+
+public class GameManager
+{
+    private GoudGame game;
+
+    public GameManager(GoudGame game) { }
+
+    public void Initialize() { }
+
+    public void Start() { }
+
+    public void Update(float deltaTime) { }
+
+    private void ResetGame() { }
+}
---
+++ goud_jumper/Program.cs
@@ -0,0 +1,18 @@
+﻿// Program.cs
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        GoudGame game = new GoudGame(800, 600, "Game Title", 60);
+        GameManager gameManager = new GameManager(game);
+
+        game.Initialize(() => gameManager.Initialize());
+        game.Start(() => gameManager.Start());
+        game.Update(() => gameManager.Update(game.UpdateResponseData.delta_time));
+
+        game.Terminate();
+    }
+}
