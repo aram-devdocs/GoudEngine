1e9363028682890984ee1889cedf42a4aefb9b0c
Aram Hammoudeh <aram.hammoudeh@indr.com>
2025-01-22 18:45:22 -0700

    test: add unit tests for logger initialization

--- goud_engine/src/libs/logger/mod.rs
+++ goud_engine/src/libs/logger/mod.rs
@@ -1,3 +1,23 @@
 pub fn init() {
     env_logger::init();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_init() {
+        // Test that init() doesn't panic
+        init();
+
+        // Log some test messages to verify logger is working
+        log::error!("Test error message");
+        log::warn!("Test warning message");
+        log::info!("Test info message");
+        log::debug!("Test debug message");
+        log::trace!("Test trace message");
+
+        // No assert needed - if initialization failed, the log macros would panic
+    }
+}
