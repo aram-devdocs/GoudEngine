05cf6358478e0eccfeb53edfd849d96d14da6fc5
Aram Hammoudeh <aram.hammoudeh@indr.com>
2025-04-27 20:54:29 -0600

    add unit tests to platform

--- goud_engine/src/libs/platform/custom_errors.rs
+++ goud_engine/src/libs/platform/custom_errors.rs
@@ -1,9 +1,20 @@
 use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq)]
 #[allow(dead_code)]
 
 pub enum Errors {
     #[error("This is a testing error for the goud_engine.")]
     TestError,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_test_error() {
+        let result = Errors::TestError;
+        assert_eq!(result, Errors::TestError);
+    }
 }
